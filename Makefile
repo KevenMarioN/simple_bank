344dba7020095f8ba9f62f7b539ed17793f11e49
KevenMarioN <kevenmario.n.r@gmail.com>
2023-05-19 16:22:48 -0300

    feat: Implementação do git
    - Configuração por env usando VIPER

--- Makefile
+++ Makefile
@@ -19,4 +19,7 @@ sqlc:
 test:
 	go test -v -cover ./...
 
-.PHONY: createdb dropdb postgres12 migrateup migratedown sqlc test
+server:
+	go run main.go
+
+.PHONY: created dropdb postgres12 migrateup migratedown sqlc test server
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/KevenMarioN/simple_bank/api"
+	db "github.com/KevenMarioN/simple_bank/db/sqlc"
+	"github.com/KevenMarioN/simple_bank/util"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	if err = server.Start(config.ServerAddress); err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
