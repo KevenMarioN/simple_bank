33723be0ae281160a5e82cca0771e8edb7e73c31
KevenMarioN <kevenmario.n.r@gmail.com>
2023-05-19 11:10:17 -0300

    fix: correção no main_test

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	dbDriver = "postgres"
-	dbSource = "postgresql://root@localhost:5432/simple_bank?sslmode=disable"
+	dbSource = "postgresql://root:123456@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
